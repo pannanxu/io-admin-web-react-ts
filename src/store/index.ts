d4265957495baffac0a7ad6874b5b44b410840d9
Administrator <admin@example.com>
2021-06-10 21:17:12 +0800

    fix: 修复redux-devtool的类型错误问题

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,6 @@ import thunkMiddleware from 'redux-thunk'
 import reducer from './reducer'
 
 // composeEnhancers 函数,让chrome的redux-devtools插件获取到数据
-// @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // 应用redux-thunk中间件
